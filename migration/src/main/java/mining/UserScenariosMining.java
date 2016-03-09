62a5ea425601f7da46ccb7ced620e168c25eace3
Felipe A. P. Pinto <felipe_app@yahoo.com.br>
2016-03-08 21:46:49 -0300

    Correção da consulta da classe UserScenariosMining.

--- migration/src/main/java/mining/UserScenariosMining.java
+++ migration/src/main/java/mining/UserScenariosMining.java
@@ -1,16 +1,15 @@
-/**
- * 
- */
 package mining;
 
-import java.util.Arrays;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Query;
 
 import domain.LogOperacao;
@@ -18,67 +17,70 @@ import domain.RegistroEntrada;
 import util.VersionMapUtil;
 
 /**
- * This class should have mining method to get information of mongodb to discovery degradation in UFRN systems
+ * This class should have mining method to get information of mongodb to
+ * discovery degradation in UFRN systems
  * 
  * @author jadson
  *
  */
 public class UserScenariosMining {
-	
 
 	/**
-	 * This method should get from the mongobd a map of ALL users and the time that each user spend to access ALL pages (scenario)
-	 * in a specific version of a system.
+	 * This method should get from the mongobd a map of ALL users and the time
+	 * that each user spend to access ALL pages (scenario) in a specific version
+	 * of a system.
 	 * 
 	 * All these data will be use for construct statistic analysis.
 	 * 
-	 *  FIXME! it is not working
-	 *  FIXME!
-	 *  FIXME!
-	 *  FIXME!
+	 * FIXME! Is it working now?
 	 * 
-	 * @param version of the system
-	 * @return A mapping have the   <user_id+scenario, {timeScenario1, timeScenario2, timeScenario3, ..., timeScenarioN}> 
+	 * @param version
+	 *            The version of the system
+	 * @return A mapping have the <user_id+scenario, {timeScenario1,
+	 *         timeScenario2, timeScenario3, ..., timeScenarioN}>
 	 */
-	public Map<String, List<Integer>> findUserScenario(String version){
-		
-		// get all log between these dates
+	public Map<String, List<Integer>> findUserScenario(String version) {
+
+		// get all log between these dates for a specific system
 		Date initialDate = VersionMapUtil.getInitialDateOfVersion(version);
 		Date finalDate = VersionMapUtil.getFinalDateOfVersion(version);
-		
-		
-		MongoTemplate mongoTemplate = MongoDatabase.buildMongoDatabaseWithMongoTemplate();
-		
-		Query query = new Query(Criteria.where("logOperacao.horario").gt(initialDate).lt(finalDate));
-		
-		List<RegistroEntrada> registros = mongoTemplate.find(query, RegistroEntrada.class);
-		
+		String system = version.substring(0, version.indexOf('-')).trim().toUpperCase();
+
+		MongoOperations mongoOps = MongoDatabase.buildMongoDatabase();
+
+		Query query = query(where("dataEntrada").gt(initialDate).lt(finalDate).and("sistema").is(system));
+
+		List<RegistroEntrada> registros = mongoOps.find(query, RegistroEntrada.class);
+
 		Map<String, List<Integer>> retorno = new HashMap<String, List<Integer>>();
-		
+
 		for (RegistroEntrada registroEntrada : registros) {
-			
-			for (LogOperacao log : registroEntrada.getLogOperacao() ){
-				
-				String key = registroEntrada.getIdUsuario()+log.getAction();
-				
-				if(retorno.containsKey(key) ){
-					retorno.get(key).add(log.getTempo());
-				}else{
-					retorno.put(key, Arrays.asList( new Integer[]{log.getTempo()}));
+
+			for (LogOperacao log : registroEntrada.getLogOperacao()) {
+
+				String key = registroEntrada.getIdUsuario() + log.getAction();
+
+				List<Integer> tempos = retorno.get(key);
+
+				if (tempos == null) {
+					tempos = new ArrayList<Integer>();
+					retorno.put(key, tempos);
 				}
+
+				tempos.add(log.getTempo());
 			}
-			
+
 		}
-		
+
 		return retorno;
 	}
 
 	public static void main(String[] args) {
-		Map<String, List<Integer>> map = new UserScenariosMining().findUserScenario("SIGAA-3.5.0");
-		
+		Map<String, List<Integer>> map = new UserScenariosMining().findUserScenario("SIPAC-3.5.0");
+
 		for (String key : map.keySet()) {
-			System.out.println(">>>>"+map.get(key));
+			System.out.println(map.get(key));
 		}
 	}
-	
+
 }
