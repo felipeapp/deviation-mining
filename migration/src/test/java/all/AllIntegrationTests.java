3128ca1c08f18ac2179c84d354396c3c2a4df464
Jadson Santos <jadson@info.ufrn.br>
2016-03-13 11:22:12 -0300

    Configurando R
    Signed-off-by: Jadson Santos <jadson@info.ufrn.br>

---
+++ migration/src/test/java/all/AllIntegrationTests.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) Automation Software Engineering Group
+ *
+ * This software is distributed WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND
+ */
+package all;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import r.JRItest;
+
+/**
+ * @author jadson - jadsonjs@gmail.com
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ JRItest.class })
+public class AllIntegrationTests {
+
+}
--- migration/src/test/java/r/JRItest.java
+++ migration/src/test/java/r/JRItest.java
@@ -5,77 +5,89 @@
  */
 package r;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.rosuda.JRI.Rengine;
 
 /**
- * This class test if Java/R integration are correctly. Afterwards you’ll see a random 
- * number from an uniform distribution. 
- * 
  * @author jadson - jadsonjs@gmail.com
  *
+ * @see http://www.codophile.com/how-to-integrate-r-with-java-using-rjava/
  * @see http://binfalse.de/2011/02/20/talking-r-through-java/
  * 
- * To run sucessifuly this class we need to install R on our machine and make some configurations. 
+ * ===== Step-1 (Download and Install R workbench)  ===== 
  * 
- *  R is ‘GNU S’, a freely available language and environment for statistical computing and graphics which provides a wide variety of statistical and graphical 
- *  techniques: linear and nonlinear modelling, statistical tests, time series analysis, classification, clustering, etc. Please consult the R project homepage for further information.
- *  
- *  CRAN is a network of ftp and web servers around the world that store identical, up-to-date, versions of code and documentation for R. Please use the CRAN mirror nearest to you to minimize network load. 
- * 
- * The follow steps need to be made:
- * 
- * === Installation ===
- * 
- * Install R on you operation system:  
- * 			Windows: https://cran.r-project.org/bin/windows/base/
- *          MacOS: https://cran.r-project.org/bin/macosx/
- *          Ubuntu: https://cran.r-project.org/bin/linux/ubuntu/README
- * 
- * JRI is now part of rJava, so if you simply want to use JRI, install rJava binary and your'e all set! 
- * 
- * Open the R terminal.
- *  The best way is to simply install rJava. It is available from CRAN, so you can use "install.packages("rJava")"
+ * https://cran.r-project.org/bin/windows/base/
+ * https://cran.r-project.org/bin/macosx/
  * 
  * 
- * === Shell environment ===
  * 
- * To talk to R through Java you have to specify three more environmental variables. First of all you need to publish you R installation path, my R is 
- * found in /usr/lib64/R : export R_HOME=/usr/lib64/R
- * In windows export C:\Program Files\R\R-3.2.4
+ * ===== Step-2 (Installing rJava package) ===== 
+ * 
+ * Open you R workbench and type following command in R console: "install.packages('rJava')"
+ * 
+ * The console will display a small window with CRAN mirror header. Choose a mirror from which rJava package will be installed 
+ * (for the purpose of this tutorial we have used mirror USA (KS)). After the package is successfully installed you will see a screen something 
+ * similar to below figure.
+ * 
+ * 
+ * 
+ * =====  Step-3 (Configuring PATH variable for rJava) ===== 
+ * 
+ *  Add JRI.jar, JRIEngine.jar, REngine.jar to the classe path (GRADLE)
+ * 
+ * In order to use rJava package inside Java code we need to add few location to the PATH environment variable. Follow following steps to configure PATH:
+ * 
+ * On Windows:
+ * 
+ * Right click on My Computer and select Properties.
+ * In the System window that appears, select Advanced system settings.
+ * System Properties dialog will appear, on this dialog select Environment Variables.
+ * Under System variables section search for variable PATH. Select PATH variable and click on Edit button.
+ * In the  Variable value section append ;D:\ProgramFiles\R\R-3.1.3\bin\i386;D:\ProgramFiles\R\R-3.1.3\library\rJava\jri; 
+ * (see the ; at the start is necessary to separate current paths from existing paths). In this step we have added two paths, 
+ * one corresponding to R dlls and other corresponding to rJava dlls. Note: As I have installed R in D:\ProgramFiles\R all my 
+ * paths contain this location for your location the generic path structure should be like ;<YOUR_R_HOME>\bin\i386;<YOUR_R_HOME>\library\rJava\jri\i386;
+ * 
+ * 
+ * On mac os:
+ * 
+ * vim .bash_profile
+ * R_HOME=/Library/Frameworks/R.framework/Resources
+ * LD_LIBRARY_PATH=/Library/Frameworks/R.framework/Resources/library/rJava/jri
+ * export :$R_HOME:$LD_LIBRARY_PATH
+ * 
  *
- * Second the $CLASSPATH needs to get an update. Precisely you have to add the archives JRIEngine.jar , JRI.jar and REngine.jar . In my case all of 
- * them can be found in /usr/lib/R/site-library/rJava/jri/ , so the $CLASSPATH should be set like that:
- *
- * export CLASSPATH=.:/usr/lib/R/site-library/rJava/jri/
- * If the $CLASSPATH isn’t defined correctly you won’t be able to compile your Java code.  (this part is configured in gradle build file)
- *
- * Last but not least you have to add the native JRI-library to your $LD_LIBRARY_PATH , by default this lib is located in the same directory like the jar’s:
- * export LD_LIBRARY_PATH=/usr/lib/R/site-library/rJava/jri/
- *
- *
- * === Eclipse setup ===
  * 
- * Instead of the $LD_LIBRARY_PATH you can set the java.library.path in Run > Run Configurations > Arguments. 
- * Add -Djava.library.path=.:/usr/lib/R/site-library/rJava/jri/ to the VM arguments (modify the path to match your criteria). The R_HOME can be published in Run > 
- * Run Configurations > Environment. Create a new variable with the name R_HOME and the value /usr/lib64/R (or an equivalent path). That’s it, see the section 
- * above to identify what went wrong if something fails.
+ *  Run > Run Configurations > Environment. Create a new variable with the name R_HOME
+ *  /Library/Frameworks/R.framework/Resources
+ *
+ *
+ *  Run > Run Configurations > JVM Arguments. Add
+ * -Djava.library.path=.:/Library/Frameworks/R.framework/Resources/library/rJava/jri
+ * 
  */
 public class JRItest {
 
-	
-	public static void main(String[] args) {
-		// new R-engine
-	    Rengine re=new Rengine (new String [] {"--vanilla"}, false, null);
-	    if (!re.waitForR())
-	    {
-	      System.out.println ("Cannot load R");
-	      return;
-	    }
-	    
-	    // print a random number from uniform distribution
-	    System.out.println (re.eval ("runif(1)").asDouble ());
+	@Test
+	public void testConnection() {
 
-	    // done...
-	    re.end();
-	}
+        // Create an R vector in the form of a string.
+        String javaVector = "c(1,2,3,4,5)";
+
+        // Start Rengine.
+        Rengine engine = new Rengine(new String[] { "--no-save" }, false, null);
+
+        // The vector that was created in JAVA context is stored in 'rVector' which is a variable in R context.
+        engine.eval("rVector=" + javaVector);
+        
+        //Calculate MEAN of vector using R syntax.
+        engine.eval("meanVal=mean(rVector)");
+        
+        //Retrieve MEAN value
+        double mean = engine.eval("meanVal").asDouble();
+        
+        //Print output values
+        Assert.assertEquals(3.0, mean, 0.0);
+    }
 }
