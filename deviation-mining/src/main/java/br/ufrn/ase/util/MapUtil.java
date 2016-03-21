c1e02f4f013e4dc126e6576e9c4e582b1f4a04ab
Felipe A. P. Pinto <felipe_app@yahoo.com.br>
2016-03-21 02:08:52 -0300

    Classe utilitária para maps.
    Signed-off-by: Felipe A. P. Pinto <felipe_app@yahoo.com.br>

---
+++ deviation-mining/src/main/java/br/ufrn/ase/util/MapUtil.java
@@ -0,0 +1,30 @@
+package br.ufrn.ase.util;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public abstract class MapUtil {
+
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValues(Map<K, V> map) {
+		List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+
+		Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+			public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+				return (o1.getValue()).compareTo(o2.getValue());
+			}
+		});
+
+		Map<K, V> result = new LinkedHashMap<K, V>();
+
+		for (Map.Entry<K, V> entry : list) {
+			result.put(entry.getKey(), entry.getValue());
+		}
+
+		return result;
+	}
+
+}
