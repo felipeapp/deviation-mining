fcc672d59c49d466657a56db2201bf93cd721e3a
Felipe A. P. Pinto <felipe_app@yahoo.com.br>
2016-04-14 19:07:27 -0300

    Removido código não usado.
    Signed-off-by: Felipe A. P. Pinto <felipe_app@yahoo.com.br>

--- deviation-mining/src/main/java/br/ufrn/ase/dao/mongodb/UserScenariosMongoDAO.java
+++ deviation-mining/src/main/java/br/ufrn/ase/dao/mongodb/UserScenariosMongoDAO.java
@@ -5,44 +5,39 @@
  */
 package br.ufrn.ase.dao.mongodb;
 
-import static org.springframework.data.mongodb.core.query.Criteria.where;
-import static org.springframework.data.mongodb.core.query.Query.query;
-
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Query;
-
 import br.ufrn.ase.dao.DAOFactory;
-import br.ufrn.ase.dao.Database;
 import br.ufrn.ase.dao.RegistroEntradaDAO;
 import br.ufrn.ase.dao.UserScenariosDAO;
 import br.ufrn.ase.domain.LogOperacao;
 import br.ufrn.ase.domain.RegistroEntrada;
-import br.ufrn.ase.util.VersionMapUtil;
 
 /**
- * This class should have mining method to get information on mongodb database.
+ * This class should have mining method to get information from mongodb
+ * database.
  * 
  * @author jadson - jadsonjs@gmail.com
  */
 public class UserScenariosMongoDAO implements UserScenariosDAO {
 
 	/**
-	 * This method should get from the mongobd a map of ALL users and the time
-	 * that each user spend to access ALL pages (scenario) in a specific version
-	 * of a system.
-	 * 
-	 * All these data will be use for construct statistic analysis.
+	 * This method gets from the mongodb a map of all users and the time that
+	 * each user spends to access all pages (scenarios) in a specific version of
+	 * a system.
 	 * 
 	 * @param system_version
-	 *            The version of the system
-	 * @return A mapping have the <user_id+scenario, {timeScenario1,
-	 *         timeScenario2, timeScenario3, ..., timeScenarioN}>
+	 *            The system and its version
+	 * @param is_user_enabled
+	 *            If true the map key will be like user_id+scenario. If false it
+	 *            will be only scenario.
+	 * @return A map like <user_id+scenario, {timeScenario1, timeScenario2,
+	 *         timeScenario3, ..., timeScenarioN}> if is_user_enabled is true,
+	 *         or <scenario, {timeScenario1, timeScenario2, timeScenario3, ...,
+	 *         timeScenarioN}> if is_user_enabled is false.
 	 */
 	public Map<String, List<Double>> findUserScenario(String system_version, boolean is_user_enabled) {
 		Map<String, List<Double>> retorno = new HashMap<String, List<Double>>();
@@ -68,55 +63,19 @@ public class UserScenariosMongoDAO implements UserScenariosDAO {
 
 		return retorno;
 	}
-	
-	
+
 	/**
-	 * This method should get from the mongobd a map of ALL users and the time
-	 * that each user spend to access ALL pages (scenario) in a specific version
-	 * of a system.
+	 * This method gets from the mongodb a map of all users and the time that
+	 * each user spends to access all pages (scenarios) in a specific version of
+	 * a system.
 	 * 
-	 * All these data will be use for construct statistic analysis.
-	 * 
-	 * @param version
-	 *            The version of the system
-	 * @return A mapping have the <user_id+scenario, {timeScenario1,
-	 *         timeScenario2, timeScenario3, ..., timeScenarioN}>
+	 * @param system_version
+	 *            The system and its version
+	 * @return A map like <user_id+scenario, {timeScenario1, timeScenario2,
+	 *         timeScenario3, ..., timeScenarioN}>
 	 */
-	public Map<String, List<Double>> findUserScenario(String version) {
-
-		// get all log between these dates for a specific system
-		Date initialDate = new VersionMapUtil().getInitialDateOfVersion(version);
-		Date finalDate = new VersionMapUtil().getFinalDateOfVersion(version);
-		String system = version.substring(0, version.indexOf('-')).trim().toUpperCase();
-
-		MongoOperations mongoOps = Database.buildMongoDatabase();
-
-		Query query = query(where("dataEntrada").gt(initialDate).lt(finalDate).and("sistema").is(system));
-		query.fields().include("idUsuario").include("logOperacao.action").include("logOperacao.tempo");
-
-		List<RegistroEntrada> registros = mongoOps.find(query, RegistroEntrada.class);
-
-		Map<String, List<Double>> retorno = new HashMap<String, List<Double>>();
-
-		for (RegistroEntrada registroEntrada : registros) {
-
-			for (LogOperacao log : registroEntrada.getLogOperacao()) {
-
-				String key = registroEntrada.getIdUsuario() + log.getAction();
-
-				List<Double> tempos = retorno.get(key);
-
-				if (tempos == null) {
-					tempos = new ArrayList<Double>();
-					retorno.put(key, tempos);
-				}
-
-				tempos.add(new Double((double) log.getTempo()));
-			}
-
-		}
-
-		return retorno;
+	public Map<String, List<Double>> findUserScenario(String system_version) {
+		return findUserScenario(system_version, true);
 	}
 
 }
