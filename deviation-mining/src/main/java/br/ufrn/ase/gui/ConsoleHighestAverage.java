6ae3daf2ced4e77fdc9d745f953342728399c0aa
Jadson Santos <jadson@info.ufrn.br>
2016-04-17 14:51:34 -0300

    calculate avetarage scenarios
    Signed-off-by: Jadson Santos <jadson@info.ufrn.br>

--- deviation-mining/src/main/java/br/ufrn/ase/gui/ConsoleHighestAverage.java
+++ deviation-mining/src/main/java/br/ufrn/ase/gui/ConsoleHighestAverage.java
@@ -5,6 +5,14 @@
  */
 package br.ufrn.ase.gui;
 
+import java.util.List;
+import java.util.Map;
+
+import br.ufrn.ase.analysis.UserScenariosStatistics;
+import br.ufrn.ase.r.GraphicPlot;
+import br.ufrn.ase.service.performance.UserScenariosService;
+import br.ufrn.ase.util.MapUtil;
+
 /**
  * @author jadson - jadsonjs@gmail.com
  *
@@ -15,7 +23,36 @@ public class ConsoleHighestAverage {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Starting ... ");
+		
+		long start = System.currentTimeMillis();
+		
+		int RANGE = 10;
+		
+		UserScenariosService userScenariosService = new UserScenariosService();
+		
+		//Map<String, List<Double>> retorno_3_20 = userScenariosService.findUserScenario("SIGAA-3.20.0", false);
+		//Map<String, Double> mapRange_3_20 = new UserScenariosStatistics().calculateCoefficientOfVariation(retorno_3_20, true);
+
+		//mapRange_3_20 = MapUtil.cutOff(mapRange_3_20, RANGE);
+		
+		Map<String, List<Double>> retorno_3_21 = userScenariosService.findUserScenario("SIGAA-3.21.0", false);
+		Map<String, Double> mapRange_3_21 = new UserScenariosStatistics().calculateExecutionMeanScenario(retorno_3_21);
+
+		mapRange_3_21 = MapUtil.cutOff(mapRange_3_21, RANGE);
+		
+		
+		GraphicPlot plot = new GraphicPlot();
+		
+		//plot.drawColumnChart(mapRange_3_20);
+		//plot.drawBoxPlotChart(mapRange_3_20);
+		
+		plot.drawColumnChart(mapRange_3_21);
+		plot.drawBoxPlotChart(mapRange_3_21);
+
+		System.out.println("Time: " + (System.currentTimeMillis() - start) / 1000.0 + " seconds");
+
 
 	}
 
