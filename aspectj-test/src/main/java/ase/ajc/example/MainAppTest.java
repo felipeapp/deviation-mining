7d953162949e1388bc82b4cb07e1137084b91706
Felipe Alves <felipe_app@yahoo.com.br>
2016-07-11 14:19:02 -0300

    Novo teste para aspectj e gradle.
    Signed-off-by: Felipe Alves <felipe_app@yahoo.com.br>

--- aspectj-test/src/main/java/ase/ajc/example/MainAppTest.java
+++ aspectj-test/src/main/java/ase/ajc/example/MainAppTest.java
@@ -1,7 +1,10 @@
 package ase.ajc.example;
 
+import br.ufrn.ppgsc.scenario.analyzer.common.annotations.arq.Scenario;
+
 public class MainAppTest {
 
+	@Scenario(name = "teste aj-gradle")
 	public static void main(String[] args) {
 		System.out.println("Main method execution!");
 		new MainAppTest().another();
