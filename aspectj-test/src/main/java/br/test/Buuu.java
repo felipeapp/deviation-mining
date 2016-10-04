7fe6c116f20c34b183b9b27b1073c18477c28b85
Felipe <Felipe@10.226.1.27>
2016-10-04 15:55:13 -0300

    Exemplo mais completo (aspectos+gradle).
    Signed-off-by: Felipe <Felipe@10.226.1.27>

---
+++ aspectj-test/src/main/java/br/test/Buuu.java
@@ -0,0 +1,9 @@
+package br.test;
+
+public class Buuu {
+
+	public static void main(String[] args) {
+		System.out.println("Buuuu");
+	}
+
+}
---
+++ aspectj-test/src/main/java/br/test/Sozinho.java
@@ -0,0 +1,5 @@
+package br.test;
+
+public class Sozinho {
+
+}
